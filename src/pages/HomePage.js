a78ef2f80990b8756e6740db2f4c9ec4cd02099a
gald10102 <gald10102@gmail.com>
2024-06-12 11:57:02 +0300

    Added background and buttons to homepage
    Added background and buttons to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,58 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import backgroundImage from '../assets/images/Background.webp';
 const HomePage = () => {
+    useEffect(() => {
+        // Disable scrolling on mount
+        document.body.style.overflow = 'hidden';
+
+        // Re-enable scrolling when component unmounts
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, []);
+
     return (
-        <div>
-            <h1 className='text-3xl font-bold underline'>Welcome to the Home Page</h1>
-            <p>This is a simple React home page.</p>
+        <div style={{
+            backgroundImage: `url(${backgroundImage})`,
+            minHeight: '100vh',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '20px',
+            textAlign: 'center',
+            color: 'white',
+            position: 'relative' // Add this line to make position:relative
+        }}>
+            <button
+                onClick={handleClick}
+                className="bg-white text-blue-500 font-bold py-4 px-10 rounded-full shadow-md hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                style={{ position: "absolute", bottom: "30%", left: "12%", transform: "translate(-50%, -50%)" }}
+                
+            >
+                Log In
+            </button>
+            <button
+                onClick={handleClick}
+                className="bg-white text-blue-500 font-bold py-4 px-10 rounded-full shadow-md hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                style={{ position: "absolute", bottom: "30%", left: "25%", transform: "translate(-50%, -50%)" }}
+            >
+                Sign Up
+            </button>
+            <button
+                onClick={handleClick}
+                className="bg-white text-blue-500 font-bold py-6 px-14 rounded-full shadow-md hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                style={{ position: "absolute", bottom: "12%", left: "66.7%", transform: "translate(-50%, -50%)" }}
+            >
+                CHAT NOW
+            </button>
         </div>
     );
 };
 
+const handleClick = () => {
+  alert("button clicked");
+}
 export default HomePage;
