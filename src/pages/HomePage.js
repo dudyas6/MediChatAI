13a899b3f6fd8485749d14e39d68c461eb0621d1
gald10102 <gald10102@gmail.com>
2024-06-12 12:06:25 +0300

    test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -46,7 +46,7 @@ const HomePage = () => {
                 className="bg-white text-blue-500 font-bold py-6 px-14 rounded-full shadow-md hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
                 style={{ position: "absolute", bottom: "12%", left: "66.7%", transform: "translate(-50%, -50%)" }}
             >
-                CHAT NOW
+                CHAT NOW hi
             </button>
         </div>
     );
