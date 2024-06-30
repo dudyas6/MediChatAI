5b3282728ff30d05c0ce6529e514f58bb6fe7c6d
David Asulin <dudyas6@gmail.com>
2024-06-30 09:10:51 +0300

    Fixed navbar on mobiles

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -24,18 +24,23 @@ const NavBar = ({ flexBetween, selectedPage, setSelectedPage }) => {
         </div>
       )}
       {!isAboveMediumScreens && (
+        <div className="flex flex-row gap-4">
+                  <UserButton />
         <button onClick={() => setIsMenuToggled((prev) => !prev)}>
           <Bars3Icon className="h-8 w-8" />
         </button>
+
+        </div>
       )}
       {/* MOBILE MENU MODAL */}
       {!isAboveMediumScreens && isMenuToggled && (
-        <div className="fixed right-0 top-0 z-40 h-80 rounded-es-3xl w-[175px] md:w-[300px] bg-secondary drop-shadow-2xl">
+        <div className="fixed right-0 top-0 z-40 h-96 rounded-es-3xl w-[175px] md:w-[300px] bg-secondary drop-shadow-2xl">
           {/* CLOSE ICON */}
           <div className="flex justify-end p-5 md:pr-16 sm:pt-10">
             <button onClick={() => setIsMenuToggled((prev) => !prev)}>
               <XMarkIcon className="h-10 w-10" />
             </button>
+            
           </div>
 
           {/* MENU ITEMS */}
@@ -44,7 +49,9 @@ const NavBar = ({ flexBetween, selectedPage, setSelectedPage }) => {
               selectedPage={selectedPage}
               setSelectedPage={setSelectedPage}
             />
+            <NavigationLink to={"/chat"} page="Chat Now" />       
           </div>
+          
         </div>
       )}
     </nav>
