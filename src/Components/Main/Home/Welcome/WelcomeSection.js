4866a04a053364f65ce01f5f235636b0ed184cde
gald10102 <gald10102@gmail.com>
2024-07-02 16:04:50 +0300

    added chatNow button in the homepage

--- src/Components/Main/Home/Welcome/WelcomeSection.js
+++ src/Components/Main/Home/Welcome/WelcomeSection.js
@@ -1,12 +1,19 @@
 import React from "react";
 import SectionWrapper from "../SectionWrapper";
 import Cards from "./Cards";
-import image from "Assets/Images/transparent_background.png"
+import image from "Assets/Images/transparent_background.png";
+import Button from "Components/UI/Button"; // Adjust the import path as per your actual file location
+
+
+const HandleClick = (e) => {
+  
+};
+
 function HomePage() {
   return (
     <SectionWrapper id="home">
-      <div className="flex flex-col-reverse  md:flex-row items-center justify-between gap-10 text-center md:text-left">
-        <div className=" tracking-wider md:tracking-normal max-w-xs lg:max-w-xl ">
+      <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-10 text-center md:text-left">
+        <div className="tracking-wider md:tracking-normal max-w-xs lg:max-w-xl">
           <h1 className="lg:text-7xl text-4xl font-bold">
             Your Health Is Our Top Priority
           </h1>
@@ -16,13 +23,15 @@ function HomePage() {
           </p>
         </div>
         <div className="max-w-xs md:max-w-none">
-          <img src= {image} width='500px' alt="hero" />
-
+          <img src={image} width="500px" alt="hero" />
         </div>
+      </div>
+      <div className="flex mt-10 px-5 py-3 text-lg">
+        <Button  onClick={(HandleClick)} >Chat Now</Button>
       </div>
       <Cards />
     </SectionWrapper>
   );
 }
-
+  
 export default HomePage;
--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 const Button = ({ children }) => {
   const nav = useNavigate();
   return (
-    <button className="bg-primary transition hover:bg-[#158ace] px-8 py-1 shadow-lg rounded-3xl text-white">
+    <button className="bg-primary transition hover:bg-[#158ace] px-10 py-5 shadow-lg rounded-3xl text-white">
       {children}
     </button>
   );
