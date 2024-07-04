534313c3ed7871e9628e82d1347ccd57f862812f
gald10102 <gald10102@gmail.com>
2024-07-04 13:57:28 +0300

    added chat now button on home page

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -1,9 +1,14 @@
+// Components/UI/Button.js
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const Button = ({ children }) => {
+const Button = ({ onClick, children }) => {
   const nav = useNavigate();
   return (
-    <button className="bg-primary transition hover:bg-[#158ace] px-10 py-5 shadow-lg rounded-3xl text-white">
+    <button
+      onClick={onClick}
+      className="bg-primary transition hover:bg-[#158ace] px-10 py-5 shadow-lg rounded-3xl text-white"
+    >
       {children}
     </button>
   );
