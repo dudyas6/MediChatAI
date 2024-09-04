96787a8ac5a12ddd0f6a0c3c5821d5b381000a1b
gald10102 <gald10102@gmail.com>
2024-09-04 15:05:33 +0300

    added more user tests

--- tests/api/user.test.js
+++ tests/api/user.test.js
@@ -41,4 +41,31 @@ test.describe('User API Tests', () => {
       message: 'Password updated successfully',
     });
   });
+
+  test('Find user valid', async ({ request }) => {
+    const response = await request.get(`${BASEURL}/user?username=Gal`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      });
+    expect(response.status()).toBe(200);
+
+    // Parse the response body as JSON
+    const responseBody = await response.json();
+    console.log(responseBody.username)
+    expect(responseBody.username).toEqual(
+      "Gal"
+    );
+  });
+
+  
+  test('Find user invalid', async ({ request }) => {
+    const response = await request.get(`${BASEURL}/user?username=Randomuser`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      });
+    expect(response.status()).toBe(400);
+  });
+
 });
